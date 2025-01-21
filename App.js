28d2f746e90fe49673dfef0f607f6f1585766b86
swc9803 <swc9803@gmail.com>
2025-01-21 16:21:35 +0900

    arcballControls

--- App.js
+++ App.js
@@ -1,91 +1,94 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/addons/Addons.js"
+import * as THREE from "three";
+import { ArcballControls } from "three/addons/Addons.js";
 
 export default class App {
-  constructor() {
-    this._setupThreeJs();
-    this._setupCamera();
-    this._setupLight();
-    this._setupControls();
-    this._setupModel();
-    this._setupEvents();
-  }
+	constructor() {
+		this._setupThreeJs();
+		this._setupCamera();
+		this._setupLight();
+		this._setupControls();
+		this._setupModel();
+		this._setupEvents();
+	}
 
-  _setupThreeJs() {
-    const divContainer = document.querySelector("#app");
-    this._divContainer = divContainer;
-    let renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor(new THREE.Color("#2c3e50"), 1);
-    renderer.setPixelRatio(window.devicePixelRatio);
+	_setupThreeJs() {
+		const divContainer = document.querySelector("#app");
+		this._divContainer = divContainer;
+		let renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setClearColor(new THREE.Color("#2c3e50"), 1);
+		renderer.setPixelRatio(window.devicePixelRatio);
 
-    divContainer.appendChild(renderer.domElement);
+		divContainer.appendChild(renderer.domElement);
 
-    this._renderer = renderer;
-    const scene = new THREE.Scene();
-    this._scene = scene;
-  }
+		this._renderer = renderer;
+		const scene = new THREE.Scene();
+		this._scene = scene;
+	}
 
-  _setupCamera() {
-    const width = this._divContainer.clientWidth;
-    const height = this._divContainer.clientHeight;
-    const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 10);
-    camera.position.z = 3;
-    this._camera = camera;
-  }
+	_setupCamera() {
+		const width = this._divContainer.clientWidth;
+		const height = this._divContainer.clientHeight;
+		const camera = new THREE.PerspectiveCamera(60, width / height, 0.01, 30);
+		camera.position.z = 18;
+		this._camera = camera;
+	}
 
-  _setupLight() {
-    const color = 0xffffff
-    const intensity = 1
-    const light = new THREE.DirectionalLight(color, intensity)
-    light.position.set(-1, 2, 4)
-    this._scene.add(light)
-  }
+	_setupLight() {
+		const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+		this._scene.add(ambientLight);
 
-  _setupModel() {
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshStandardMaterial();
-    const mesh = new THREE.Mesh(geometry, material)
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+		directionalLight.position.set(5, 3, 5);
+		this._scene.add(directionalLight);
+	}
 
-    this._scene.add(mesh);
-    this._mesh = mesh;
-  }
+	_setupModel() {
+		const radius = 7;
+		const earthGeometry = new THREE.SphereGeometry(radius, 64, 32);
+		const earthMaterial = new THREE.MeshStandardMaterial({
+			map: new THREE.TextureLoader().load("./earth_day_4096.jpg"),
+		});
+		const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+		this._scene.add(earth);
+		this._earth = earth;
+	}
 
-  _setupControls() {
-    const orbitControls = new OrbitControls(this._camera, this._divContainer);
-    this._orbitControls = orbitControls;
-  }
+	_setupControls() {
+		const arcballControls = new ArcballControls(this._camera, this._divContainer, this._scene);
+		arcballControls.enablePan = false;
 
-  _setupEvents() {
-    window.onresize = this.resize.bind(this);
-    this.resize();
+		this.arcballControls = arcballControls;
+	}
 
-    this._clock = new THREE.Clock()
-    requestAnimationFrame(this.render.bind(this));
-  }
+	_setupEvents() {
+		window.onresize = this.resize.bind(this);
+		this.resize();
 
-  update() {
-    const delta = this._clock.getDelta();
+		this._clock = new THREE.Clock();
+		requestAnimationFrame(this.render.bind(this));
+	}
 
-    this._mesh.rotation.y += delta;
+	update() {
+		const delta = this._clock.getDelta();
 
-    this._orbitControls.update();
-  }
+		this.arcballControls.update();
+	}
 
-  render() {
-    this.update();
+	render() {
+		this.update();
 
-    this._renderer.render(this._scene, this._camera);
+		this._renderer.render(this._scene, this._camera);
 
-    requestAnimationFrame(this.render.bind(this));
-  }
+		requestAnimationFrame(this.render.bind(this));
+	}
 
-  resize() {
-    const width = this._divContainer.clientWidth;
-    const height = this._divContainer.clientHeight;
+	resize() {
+		const width = this._divContainer.clientWidth;
+		const height = this._divContainer.clientHeight;
 
-    this._camera.aspect = width / height;
-    this._camera.updateProjectionMatrix();
+		this._camera.aspect = width / height;
+		this._camera.updateProjectionMatrix();
 
-    this._renderer.setSize(width, height);
-  }
+		this._renderer.setSize(width, height);
+	}
 }
